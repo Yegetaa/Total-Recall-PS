4c50ad915a9c54a5b1caee76e7d901f6618098bf
Yegeta <149103185+Yegetaa@users.noreply.github.com>
2023-11-21 09:34:24 -0500

    1B completed

--- Section1/src/index.js
+++ Section1/src/index.js
@@ -12,3 +12,14 @@ console.log(`My name is ${myName} and my favorite programming language is ${favo
 
 // Exercise B. Strings 
 
+let firstVariable = "Hello World"
+firstVariable = 10
+
+let secondVariable = firstVariable
+console.log(firstVariable);
+
+let yourName = "Yegeta"
+console.log(`Hello, my name is ${yourName}.`);
+
+// Exercise C. Booleans 
+
