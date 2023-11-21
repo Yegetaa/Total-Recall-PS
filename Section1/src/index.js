60bf2887d6274c275dc32bfc58567e9af6f5bd2d
Yegeta <149103185+Yegetaa@users.noreply.github.com>
2023-11-21 09:25:52 -0500

    1A completed

---
+++ Section1/src/index.js
@@ -0,0 +1,14 @@
+// Variables and Data Types 
+
+// Exercise A. Variables 
+
+const myName = "Yegeta Eshetu"
+let favoriteLanguage = "Python"
+let newFavoriteLanguage = "JavaScript"
+
+favoriteLanguage = newFavoriteLanguage
+console.log(favoriteLanguage);
+console.log(`My name is ${myName} and my favorite programming language is ${favoriteLanguage}.`);
+
+// Exercise B. Strings 
+
